9cb492a659c0866ebb26c042638bb46d22529321
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-01-20 14:32:23 -0600

    video 4 rutas

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,24 @@ Route::get('usuario/{nombre}/comentario/{comentariod}', function($nombre="Mundo"
 });
 
 Route::get('user/{nombre}', function($nombre){
-    return 'Usuario es '.nombre;
-})->where('nombre','[A-Za-Z]+');
+    return 'Usuario es '.$nombre;
+})->where('nombre','[A-Za-z]+'); // en minuscula z del final
+
+Route::get('user2/{id}/{nombre}', function($id, $nombre){
+    return 'Usuario es '.$id. ' y el nombre es: '.$nombre;
+})->where(
+    [
+        'id' => '[0-9]+',
+        'nombre' => '[A-Za-z]+'
+    ]
+); // http://127.0.0.1:8000/user2/12/axel
+
+Route::get('prueba', function(){
+    return 'Página de prueba';
+})->name('pruebaRoute');
+
+Route::get('redirigirPrueba', function(){
+    return redirect()->route('pruebaRoute');
+});
+
+Route::redirect('/prueba3','/prueba');
